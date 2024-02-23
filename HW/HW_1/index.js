2744033d0f562851d6d49180d81b1afdcb6f904e
tymurkabulov <tymurkabulov0@gmail.com>
2024-02-23 15:44:22 +0200

    added hw1

---
+++ HW/HW_1/index.js
@@ -0,0 +1,31 @@
+
+let hello = 'hello';
+let owu = 'owu';
+let com = 'com';
+let ua = 'ua';
+let num1 = 1;
+let num2 = 10;
+let num3 = -999;
+let num4 = 123;
+let num5 = 3.14;
+let num6 = 2.7;
+let num7 = 16;
+let word = true;
+let letter = false;
+
+console.log(hello, owu, com, ua, num1, num2, num3, num4, num5, num6, num7, word, letter);
+
+
+
+let firstName = 'Tymur';
+let middleName = 'Faizulayovich';
+let lastName = 'Kabulov';
+
+let person = lastName + ' ' + firstName + ' ' + middleName;
+console.log(person);
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a + ',', typeof b + ',', typeof c);
