554deebeee7d9538f7f1cce7ff73add964a43c0f
tymurkabulov <tymurkabulov0@gmail.com>
2024-02-24 22:18:19 +0200

    added hw1

--- HW/HW_2/index.js
+++ HW/HW_2/index.js
@@ -94,7 +94,17 @@
         },
     ]
 
-    console.log(users);
+    console.log(users[0].password);
+    console.log(users[1].password);
+    console.log(users[2].password);
+    console.log(users[3].password);
+    console.log(users[4].password);
+    console.log(users[5].password);
+    console.log(users[6].password);
+    console.log(users[7].password);
+    console.log(users[8].password);
+    console.log(users[9].password);
+
 
 `Якщо змінна x не дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Невірно'. Перевірте  скрипт при a, що дорівнює 1, 0, -3`
             let x = 5;
