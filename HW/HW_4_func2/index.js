fe9e7e10d8d3b2972e9cafb085b09fd49556ce8c
tymurkabulov <tymurkabulov0@gmail.com>
2024-03-03 12:06:15 +0200

    added hw4_function2

--- HW/HW_4_func2/index.js
+++ HW/HW_4_func2/index.js
@@ -1,40 +1,37 @@
     //- створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б`
-    function areaOfRectangle(a,b){
-        return `Area of rectangle: ${a * b}`
-    }
-    console.log(areaOfRectangle(2,2))
+
+    const areaOfRectangle = (a,b) => `Area of rectangle: ${a * b}`;
+    console.log(areaOfRectangle(4, 2));
 
     //  - створити функцію яка обчислює та повертає площу кола з радіусом r`
-    function areaOfCircle(r){
-        return `Area of circle: ${3.14 * Math.pow(r,2)}`
-    }
+
+    const areaOfCircle = (r) => `Area of circle: ${3.14 * Math.pow(r,2)}`
     console.log(areaOfCircle(2));
 
     //  - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r`
-    function areaOfCylinder(r,h){
-        return `Area of cylinder: ${2 * 3.14 * r * h}`
-    }
+
+    const areaOfCylinder = (r,h) => `Area of cylinder: ${2 * 3.14 * r * h}`;
     console.log(areaOfCylinder(2,2));
 
     //  - створити функцію яка приймає масив та виводить кожен його елемент`
-    let arr = [1, 2, 3, 4, 5, 'asd', 'qwe', 'zxc'];
-    function logArray(arr){
+
+    const logArray = (arr) => {
         for (let i = 0; i < arr.length;i++){
             console.log(arr[i]);
         }
     }
 
-    logArray(arr);
+   logArray([1, 2, 3, 4, 5, 'asd', 'qwe', 'zxc']);
 
     //  - створити функцію яка повертає найменьше число з масиву`
+
     let numArr = [12, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    function minValue (){
-        return Math.min(...numArr);
-    }
+    const minValue = (numArr) => Math.min(...numArr);
     console.log(minValue(numArr));
 
     //  - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13`
-    function sumArr (numArr){
+
+    const sumArr = (numArr) => {
         let sum = 0;
         for (let i = 0;i < numArr.length;i++){
             sum += numArr[i]
@@ -42,12 +39,14 @@
         return sum;
     }
     console.log(sumArr(numArr));
+
     //  - створити функцію swap(arr,index1,index2). Функція міняє місцями значення у відаовідних індексах
     //  Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
+
     let arr2 = [11,22,33,44];
     let index1 = 0;
     let index2 = 1;
-    function swap(arr2,index1,index2){
+    const swap = (arr2,index1,index2) => {
         let x = arr2[index1];
         arr2[index1] = arr2[index2];
         arr2[index2] = x;
@@ -62,7 +61,7 @@
     let currencyValues = [{currency:'USD',value:40},{currency:'EUR',value:42}];
     let exchangeValues = 'USD';
 
-    function exchange(sumUAH, currencyValues, exchangeValues){
+    const exchange = (sumUAH, currencyValues, exchangeValues) => {
         let result = 0;
         let isValid = false;
         for (let i = 0;i < currencyValues.length;i++){
@@ -79,3 +78,4 @@
     }
 
     console.log(exchange(sumUAH, currencyValues, exchangeValues));
+
